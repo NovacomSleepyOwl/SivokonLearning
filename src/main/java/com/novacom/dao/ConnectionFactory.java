1bb077f58962201187e17ce6bf8df5e6545fb51d
Andrew Sivokon <sleepy_owl@list.ru>
2017-04-05 17:39:18 +0300

    Dao is singleton and have factory method

--- src/main/java/com/novacom/dao/ConnectionFactory.java
+++ src/main/java/com/novacom/dao/ConnectionFactory.java
@@ -5,15 +5,14 @@ import java.sql.*;
 /**
  * Created by A.Sivakon on 03.04.2017.
  */
-public class Connector {
+public class ConnectionFactory {
 
     private final String HOST = "jdbc:postgresql://localhost:5432/testdb";
     private final String USERNAME = "postgres";
     private final String PASSWORD = "root";
+    Connection connection;
 
-    private Connection connection;
-
-    public Connector(){
+    public ConnectionFactory(){
 
         try {
             Class.forName("org.postgresql.Driver");
@@ -23,14 +22,14 @@ public class Connector {
             return;
         }
 
+    }
+
+    public Connection getConnection() {
         try{
             connection = DriverManager.getConnection(HOST, USERNAME, PASSWORD);
         }catch (SQLException e){
             e.printStackTrace();
         }
-    }
-
-    public Connection getConnection() {
         return connection;
     }
 }
--- src/main/java/com/novacom/dao/Dao.java
+++ src/main/java/com/novacom/dao/Dao.java
@@ -1,20 +1,40 @@
 package com.novacom.dao;
 
-import com.novacom.dao.beans.ClientBean;
-
 import java.sql.*;
 
 /**
  * Created by A.Sivakon on 03.04.2017.
  */
 public class Dao {
+    ConnectionFactory connection;
 
+    //Block singleton-------------------------------------------
+    private static Dao uniqueInstance = new Dao();
+
+    private Dao(){
+        connection = new ConnectionFactory();
+    }
+
+    public static Dao getUniqueInstance() {
+        return uniqueInstance;
+    }
+    //----------------------------------------------------------
+
+    //Factory method------------------------------------------
+    private ConnectionFactory getConnectionFactory(){
+        return connection;
+    }
+    //----------------------------------------------------------
+
+
+
+
+    //Custom query (return first client from database)
     public void ExQuery(){
-        Connector connector = new Connector();
+
         String query = "SELECT c_name FROM clients WHERE c_id = 1";
 
-        try{
-            Statement statement = connector.getConnection().createStatement();
+        try(Statement statement = connection.getConnection().createStatement()){
             ResultSet resultSet = statement.executeQuery(query);
             while (resultSet.next()) {
                 String cname = resultSet.getString(1);
